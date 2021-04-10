662879843a00b104e1265bddbda3f9240fd8a96a
Adrien Bellamy <adrien.bellamy@hec.edu>
2021-04-10 11:45:19 +0200

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,9 +3,9 @@ class QuestionsController < ApplicationController
   def ask
   end
 
-  def answer
-    if params[:question]
-      @question = params[:question]
-    end
-  end
+  # def answer
+  #   if params[:question]
+  #     @question = params[:question]
+  #   end
+  # end
 end
